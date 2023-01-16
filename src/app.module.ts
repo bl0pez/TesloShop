6c85475061796f9d8fed59f0d296d91557083d50
b0pez <blopez4434@gmail.com>
2023-01-16 14:17:53 -0300

    configurar variables de entorno y orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    // Configuracion de variables de entorno
+    ConfigModule.forRoot(),
+    // Configuracion de la base de datos
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      database: process.env.DB_NAME,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      // En produccion no se debe usar la sincronizacion
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
